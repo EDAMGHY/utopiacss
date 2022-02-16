70208975cdf516922fbb47d9eaf0cf26637fefc7
Abdellah DAMRI <damriabdellah@gmail.com>
2022-02-16 19:22:00 +0100

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+import '../styles/main.scss';
+
+function utopia() {
+  const subMenuHeadingClass = 'utopia-submenu-heading';
+  const expandedClass = 'expanded';
+  const collapsedClass = 'collapsed';
+  const subMenuHeadings = document.getElementsByClassName(subMenuHeadingClass);
+  Array.from(subMenuHeadings).forEach(function (subMenuHeading) {
+    subMenuHeading.classList.add(collapsedClass);
+    subMenuHeading.nextElementSibling.style.display = 'none';
+    subMenuHeading.addEventListener('click', function (event) {
+      event.preventDefault();
+      const subMenuList = event.target.nextElementSibling;
+      if (subMenuList.style.display == 'none') {
+        subMenuHeading.classList.remove(collapsedClass);
+        subMenuHeading.classList.add(expandedClass);
+        subMenuList.style.display = 'block';
+      } else {
+        subMenuHeading.classList.remove(expandedClass);
+        subMenuHeading.classList.add(collapsedClass);
+        subMenuList.style.display = 'none';
+      }
+      event.stopPropagation();
+    });
+  });
+}
+
+export default utopia;
